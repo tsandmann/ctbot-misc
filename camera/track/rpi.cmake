ae716c4c84d7df6826a0d82963f1b8091eaf03d4
Timo Sandmann <mail@timosandmann.de>
2016-10-03 19:50:45 +0200

    toolchain settings updated

--- camera/track/rpi.cmake
+++ camera/track/rpi.cmake
@@ -4,15 +4,15 @@ set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 
 set(BUILD_SHARED_LIBS OFF)
 
-set(CMAKE_FIND_ROOT_PATH "/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot")
+set(CMAKE_SYSROOT "/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot")
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_SYSROOT "/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot")
 set(CMAKE_C_COMPILER "armv8l-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "armv8l-linux-gnueabihf-g++")
-set(BOOST_ROOT "/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr")
+set(BOOST_ROOT "${CMAKE_SYSROOT}")
 
 
 SET(LOCAL_INCLUDE "${CMAKE_SYSROOT}/usr/local/include")
